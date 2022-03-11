6790254d8797e5528275cabc32b730240bbe580b
s979683 <s979683@NB3031925407>
2022-03-11 08:52:17 +0400

    updates in item.jsx file

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 
 function ProductItem({element}) {
   const [toast, settoast] = useState(false);
+  const stars = Math.round(element.rating.rate) || 0;
 
+  // addedToCart
   const addedToCart = (item) => {
     console.log(item);
     settoast((state) => {
@@ -15,8 +17,9 @@ function ProductItem({element}) {
         return !state;
       });
     }, 3000);
-
   }
+  
+
   return (  
     <>
       <div className="py-6 px-3" key={element.id}>
@@ -27,10 +30,8 @@ function ProductItem({element}) {
             <h1 className="text-gray-900 font-bold text-2xl">{element.title}</h1>
             <p className="mt-2 text-gray-600 text-sm">{element.description}</p>
             <div className="flex item-center mt-2">
-              <svg className="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
-                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-              </svg>
-              <svg className="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
+              {/* starts */}
+              {/* <svg className="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
                 <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
               </svg>
               <svg className="w-5 h-5 fill-current text-gray-700" viewBox="0 0 24 24">
@@ -41,7 +42,7 @@ function ProductItem({element}) {
               </svg>
               <svg className="w-5 h-5 fill-current text-gray-500" viewBox="0 0 24 24">
                 <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
-              </svg>
+              </svg> */}
             </div>
             <div className="flex item-center justify-between mt-3">
               <h1 className="text-gray-700 font-bold text-xl">AED {element.price}</h1>
